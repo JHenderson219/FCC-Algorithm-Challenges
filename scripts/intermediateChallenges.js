4707e24b611717a4e47908c5bd4f83b1847b22d5
JHenderson219 <hendersonj219@gmail.com>
2016-10-26 10:46:35 -0700

    Completed Sum All Numbers in Range

---
+++ scripts/intermediateChallenges.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() { 	
+	/*var anArr = [];
+	function sumAll(arr) {
+		
+		if (arr[0]>arr[1]){
+    		for(var i=arr[1]; i<arr[0]+1; i++){
+    			console.log (anArr);
+    			anArr.push(i);
+				console.log (anArr);
+    		}	
+    		return anArr.reduce(function(a,b){
+    			return a+b;
+    		});
+  		} else if (arr [1]>arr[0]){
+ 
+    		for(var i=arr[0]; i<arr[1]+1; i++){
+    			console.log (anArr);
+    			anArr.push(i);
+    			console.log(anArr);
+    		}	
+    		return anArr.reduce(function(a,b){
+    			return a+b;
+    		});
+  		}
+  			return "Failed!";
+		}
+
+	document.getElementById("#output").innerHTML = "<h3 class = 'text-center'>"+sumAll([5, 10]) + "</h3>";*/
+	function diffArray(arr1, arr2) {
+  		var newArr = [];
+  		// Same, same; but different.
+  		return newArr;
+}
+
+
+	document.getElementById("#output").innerHTML = "<h3 class = 'text-center'>"+sumAll([5, 10]) + "</h3>";
+});
