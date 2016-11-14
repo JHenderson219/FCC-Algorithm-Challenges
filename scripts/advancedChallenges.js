f1bc0db97e6589dd1e279454f212aca8abb6c45e
Jordan Henderson <hendersonj219@gmail.com>
2016-11-14 14:44:57 -0800

    Working on validating US Telephone numbers

---
+++ scripts/advancedChallenges.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() { 	
+function telephoneCheck(str){
+	var validFirst = /[\d()]/gi;
+	var validChars = /[0-9]/gi;
+	debugger;
+	for(var i = 0; i<str.length; i++){
+		if (str[i] ==! validChars){
+			console.log("Invalid Characters Detected! Returned False!")
+			return false
+		}
+	}
+console.log("Returned True!")
+return true;
+}
+
+document.getElementById("#output").innerHTML = "<h3 class = 'text-center'>"
++telephoneCheck("555 555 5555");
++ "</h3>";
+});
