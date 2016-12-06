2ef8479838339e729354ad335cd0b4a0f6c95415
JHenderson219 <hendersonj219@gmail.com>
2016-12-05 17:02:18 -0800

    Finished Longest Word, working on Letter Changes

--- scripts/coderbyteChallenges.js
+++ scripts/coderbyteChallenges.js
@@ -1,8 +1,55 @@
 document.addEventListener("DOMContentLoaded", function() {  
-
-function factorialize(num){
-    return num;
+//Factorializes a number
+/*function factorialize(num){
+    debugger;
+    if (num===0){
+        return 1;
+    } else{
+        return num * factorialize(num-1);
+    }
+}*/
+//Finds longest word in a string
+function longestWord (sen){
+    var currentLongest = "";
+    var currentLength = 0;
+    var punc = /[-!$%#^&*()_+|~=`{}\[\]:";'<>?,.\/]/gi;
+    var arr = sen.split(" ");
+    var noPuncArr = [];
+    for (var i = 0; i<arr.length;i++){
+        noPuncArr.push(arr[i].replace(punc,""));
+    }
+    for (var j=0; j<noPuncArr.length-1;j++){
+        if (noPuncArr[j].length>currentLength){
+            currentLongest=noPuncArr[j];
+            currentLength=noPuncArr[j].length;
+        }
+    }
+    return currentLongest;
 }
 
-document.getElementById("#output").innerHTML = "<h3 class = 'text-center'>"+factorialize(4)+"</h3>";
+function LetterChanges (str){
+    var arr = str.split("");
+    var symbols = /[-!$%#^&*()_+|~=`{}\[\]:";'<>?,.\/]/gi;
+    var numbers = /\d/gi;
+    var codeArr = [];
+    for (var i = 0; i<arr.length-1; i++){
+        if (arr[i]===symbols){
+            codeArr.push(arr[i]);
+        } else if (arr[i] !== numbers) {
+        codeArr.push(arr[i].charCodeAt());
+        }
+    }
+    console.log (codeArr);
+    codeArr=codeArr.map(function(ind){
+        return ind+1;
+    });
+    console.log (codeArr);
+
+return str;
+}
+
+
+
+
+document.getElementById("#output").innerHTML = "<h3 class = 'text-center'>"+LetterChanges("hello*3")+"</h3>";
 });
