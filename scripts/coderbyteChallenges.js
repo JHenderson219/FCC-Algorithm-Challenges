38c1da7de615650f8cd56a943f5a751901700106
JHenderson219 <hendersonj219@gmail.com>
2016-12-06 10:06:40 -0800

    Completed Letter Changes

--- scripts/coderbyteChallenges.js
+++ scripts/coderbyteChallenges.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {  
 //Factorializes a number
-/*function factorialize(num){
+function factorialize(num){
     debugger;
     if (num===0){
         return 1;
     } else{
         return num * factorialize(num-1);
     }
-}*/
+}
+
 //Finds longest word in a string
 function longestWord (sen){
     var currentLongest = "";
@@ -29,9 +30,9 @@ function longestWord (sen){
 
 function LetterChanges (str){
     var arr = str.toLowerCase().split("");
-    var symbols = /[-!$%#^&*()_+|~=`{}\[\]:";'<>?,.\/]/g;
-    var numbers = /[\d]/g;
-    var vowels = /[aeiou]/g;
+    var symbols = /[-!\s$%#^&*()_+|~=`{}\[\]:";'<>?,.\/]/;
+    var numbers = /[\d]/;
+    var vowels = /[aeiou]/;
     var codeArr = [];
     debugger;
     for (var i = 0; i<arr.length; i++){
@@ -40,20 +41,19 @@ function LetterChanges (str){
         } else {
             var mod = arr[i].charCodeAt()+1;
             mod = String.fromCharCode(mod);
-
-            if (vowels.test(mod)){
-                mod.toUpperCase();
+            if (mod === "{"){
+                mod = "A";
+            } else if (vowels.test(mod)){
+              mod = mod.toUpperCase();
             }
-
-            codeArr.push(String.fromCharCode(mod));
+            codeArr.push(mod);
         }
     }
-
 return codeArr.join("");
 }
 
 
 
 
-document.getElementById("#output").innerHTML = "<h3 class = 'text-center'>"+LetterChanges("hello*3z")+"</h3>";
+document.getElementById("#output").innerHTML = "<h3 class = 'text-center'>"+LetterChanges("replace!*")+"</h3>";
 });
