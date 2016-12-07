8ed2b32e66577f6c6131f26b6602484e8c4e7a55
JHenderson219 <hendersonj219@gmail.com>
2016-12-06 19:56:33 -0800

    Completed Longest Consec

--- scripts/codewarsChallenges.js
+++ scripts/codewarsChallenges.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {  
-//
+//returns longest set of k consecutive strings in a given array (strarr)
 function longestConsec (strarr, k){
     var n = strarr.length;
-    debugger;
+    var longest=0;
+    var targetIndex=0;
+    var calcArr=[];
+    var calc=0;
+    var result;
     if(k>n || n===0 || k<=0){
         return "";
     } else{
@@ -10,10 +14,25 @@ function longestConsec (strarr, k){
     for (var i=0;i<strarr.length;i++){
         arrLengths.push(strarr[i].length);
     }
-    console.log(arrLengths);
-    return strarr;
+    debugger;
+    for (var j=0;j<arrLengths.length;j++){
+        calcArr = arrLengths.slice(j,j+k);
+        calc = calcArr.reduce(function(a,b){
+            return a+b;
+        },0);
+        if (calc>longest){
+            targetIndex = j;
+            longest=calc;
+        }
+    }
+    console.log (targetIndex+k-1);
+    result = strarr.slice(targetIndex,targetIndex+k)
+    result = result.join("");
+    return result;
     }
 }
-
-document.getElementById("#output").innerHTML = "<h3 class = 'text-center'>"+longestConsec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2)+"</h3>";
+function gap (g, m, n){
+    
+}
+document.getElementById("#output").innerHTML = "<h3 class = 'text-center'>"+gap(2,100,110)+"</h3>";
 });
